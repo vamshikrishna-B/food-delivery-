5c0d97b74b57f9c27baa2a9a87948078c183de53
vamshi krishna -B <bvamshikrishna1912@gmail.com>
2026-03-05 10:47:06 +0530

    Update backend files and remove db.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDb from './config/db.js';
-
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  connectDb ()
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
